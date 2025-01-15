112775b1086f79283a47890be226e5deaf55bd45
jacobboud <jacobboud2020@gmail.com>
2025-01-15 12:34:27 -0700

    Add project files.

---
+++ Mission2Methods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    internal class Mission2Methods
+    {
+
+        public static int[] diceRolls(int numRolls)
+        {
+            int[] diceRollsCount = new int[11];
+            Random random = new Random();
+            int die1 = 0;
+            int die2 = 0;
+            int value = 0;
+
+            for (int i = 0; i < numRolls; i++)
+            {
+                die1 = random.Next(1, 7);
+                die2 = random.Next(1, 7);
+                value = die1 + die2;
+
+                diceRollsCount[value - 2]++;
+            }
+
+            return diceRollsCount;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using Mission2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        System.Console.WriteLine("Welcome to the dice throwing simulator!\n\nHow many dice rolls would you like to simulate? ");
+
+        int numRolls = 0;
+        numRolls = int.Parse(System.Console.ReadLine());
+        int[] diceRollsCount = Mission2Methods.diceRolls(numRolls);
+
+        System.Console.WriteLine("\n\nDICE ROLLING SIMULATION RESULTS\nEach \"*\" represents 1% of the total number of rolls.\nTotal number of rolls = " + numRolls + "\n\n");
+
+        string[] asterisksArray = Asterisks(numRolls, diceRollsCount);
+
+        for (int num = 0; num < asterisksArray.Length; num++)
+        {
+            System.Console.WriteLine((num+2) + ": " + asterisksArray[num]);
+        }
+
+        System.Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+
+    }
+
+    public static string[] Asterisks(int rolls, int[] diceRollsCount)
+    {
+        string[] asterisksArray = new string[diceRollsCount.Length];
+        int numAsterisks = 0;
+        string asterisks = "";
+
+        for (int num = 0; num < diceRollsCount.Length; num++)
+        {
+            asterisks = "";
+            numAsterisks = (int)Math.Round(((double)diceRollsCount[num] / rolls) * 100);
+
+            for (int a = 0; a < numAsterisks; a++)
+            {
+                asterisks += "*";
+            }
+
+            asterisksArray[num] = asterisks;
+
+        }
+
+        return asterisksArray;
+
+    }
+
+}
